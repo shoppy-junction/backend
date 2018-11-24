ab3bdd7b0d008b75583bea4a3c6d9108384a5d9a
kyeb <kyeb@mit.edu>
2018-11-24 15:41:57 +0200

    add some real ish data

--- processing/process.py
+++ processing/process.py
@@ -5,18 +5,24 @@ import numpy
 
 
 BEACONS = {
-        1: (4.5,1),
-        3: (17.3,3),
-        4: (17.4,21.4),
-        5: (16,15.2),
-        6: (13.2,15.3),
+        1: (4.5,1.1),
+        3: (17.3,3.2),
+        4: (17.4,21.42),
+        5: (16,15.26),
+        6: (13.2,15.34),
         7: (10,15.31),
         8: (7,15.29),
         9: (4,15),
-        10:(3,21),
+        10:(3,21.05),
         11:(3.5,6.5),
         12:(13,6)
         }
+
+def closest_beacon(row):
+    if row.beacon_id in BEACONS:
+        return BEACONS[row.beacon_id]
+    else:
+        return None
 
 def triangulate(beacons):
     """
@@ -24,12 +30,19 @@ def triangulate(beacons):
     source: https://gist.github.com/kdzwinel/8235348
     """
 
-    x1 = BEACONS[beacons.beacon_id1][0]
-    y1 = BEACONS[beacons.beacon_id1][1]
-    x2 = BEACONS[beacons.beacon_id2][0]
-    y2 = BEACONS[beacons.beacon_id2][1]
-    x3 = BEACONS[beacons.beacon_id3][0]
-    y3 = BEACONS[beacons.beacon_id3][1]
+    if len(beacons) != 6:
+        return None
+
+    try:
+        x1 = BEACONS[beacons.beacon_id1][0]
+        y1 = BEACONS[beacons.beacon_id1][1]
+        x2 = BEACONS[beacons.beacon_id2][0]
+        y2 = BEACONS[beacons.beacon_id2][1]
+        x3 = BEACONS[beacons.beacon_id3][0]
+        y3 = BEACONS[beacons.beacon_id3][1]
+    except KeyError as e:
+        print('beacon not found:', e)
+        return None
 
     def d_from_rssi(rssi):
         return math.pow(10, (rssi+55)/-10.5)
@@ -38,19 +51,27 @@ def triangulate(beacons):
     r2 = d_from_rssi(beacons.rssi2)
     r3 = d_from_rssi(beacons.rssi3)
 
-    S = (math.pow(x3, 2) - math.pow(x2, 2) + math.pow(y3, 2) - math.pow(y2, 2) + math.pow(r2, 2) - math.pow(r3, 2)) / 2
-    T = (math.pow(x1, 2) - math.pow(x2, 2) + math.pow(y1, 2) - math.pow(y2, 2) + math.pow(r2, 2) - math.pow(r1, 2)) / 2
-    y = ((T * (x2-x3)) - (S * (x2-x1))) / (((y1-y2)*(x2-x3)) - ((y3-y2)*(x2-x1)))
+    S = (math.pow(x3, 2)-math.pow(x2, 2)+math.pow(y3, 2)-math.pow(y2, 2)+math.pow(r2, 2)-math.pow(r3, 2))/2
+    T = (math.pow(x1, 2)-math.pow(x2, 2)+math.pow(y1, 2)-math.pow(y2, 2)+math.pow(r2, 2)-math.pow(r1, 2))/2
+    try:
+        y = ((T * (x2-x3)) - (S * (x2-x1))) / (((y1-y2)*(x2-x3)) - ((y3-y2)*(x2-x1)))
+    except ZeroDivisionError:
+        return None
     x = ((y * (y1-y2)) - T) / (x2-x1)
     return x, y
 
 def process(filename):
     df = pd.read_csv(filename)
     # take closest beacon
-    # grouped = df.sort_values(['userid', 'timestamp', 'rssi']).groupby(['userid', 'timestamp'])
-    # df = grouped.tail(1)
-    # df['pos'] = df.apply(lambda row: BEACONS[row.beacon_id], axis=1)
+    grouped = df.sort_values(['userid', 'timestamp', 'rssi']).groupby(['userid', 'timestamp'])
+    df = grouped.tail(1)
+    pos = df.set_index(['userid', 'timestamp']).apply(closest_beacon, axis=1)
+    df = df.set_index(['userid', 'timestamp'])
+    df['x'], df['y'] = pos.apply(lambda x: x[0]), pos.apply(lambda x: x[1])
+    
+    return df[['x', 'y']]
 
+    """ triangulation shit
     # take triangulated value
     top3 = df.groupby(['timestamp', 'userid'])['rssi', 'beacon_id'] \
             .apply(lambda x: x.nlargest(3, columns=['rssi']) \
@@ -59,9 +80,14 @@ def process(filename):
 
     top3.columns = [col[0] + str(1+col[1]) for col in top3.columns.values]
 
+    #print(top3)
     df = top3.apply(triangulate, axis=1).reset_index()
+    #print(df)
+    df.fillna(value=pd.np.nan, inplace=True)
+    df.dropna(inplace=True)
     df['x'], df['y'] = df.apply(lambda x: x[0][0], axis=1), df.apply(lambda x: x[0][1], axis=1)
     return df.set_index(['userid', 'timestamp'])[['x', 'y']]
+    """
 
 if __name__ == "__main__":
     filename = "../data/fake_data.csv"
--- server.py
+++ server.py
@@ -16,9 +16,8 @@ def home():
 @app.route('/locationdata', methods=['POST'])
 def post_location():
     data = request.get_data(as_text=True)
-    print(type(data))
     df = pd.read_csv(StringIO(data))
-    df.columns = ['beacon_id', 'rssi', 'timestamp', 'userid']
+    df.columns = ['beacon_id', 'rssi', 'userid', 'timestamp']
     data = df
 
     conn = sqlite3.connect('beacon_data.db')
@@ -31,6 +30,11 @@ def post_location():
 
 @app.route('/locationdata', methods=['GET'])
 def get_location():
+    """
+    get endpoint
+    params (optional): 
+    - userid: userid to return results for
+    """
     conn = sqlite3.connect('beacon_data.db')
     c = conn.cursor()
     userid = request.args.get('userid')
@@ -44,11 +48,6 @@ def get_location():
 
 @app.route('/createloctable', methods=['GET'])
 def create_location_table():
-    """
-    get endpoint
-    params (optional): 
-    - userid: userid to return results for
-    """
     conn = sqlite3.connect('beacon_data.db')
     c = conn.cursor()
     try:
@@ -58,6 +57,91 @@ def create_location_table():
         c.execute("CREATE TABLE proximity (timestamp int, userid int, beacon_id int, rssi int);")
     conn.commit()
     conn.close()
-    return "table created"
+    return "location table created"
+
+@app.route('/createpurchasetable', methods=['GET'])
+def create_purchase_table():
+    conn = sqlite3.connect('beacon_data.db')
+    c = conn.cursor()
+    try:
+        c.execute("CREATE TABLE purchases (timestamp int, userid int, ean str, price str);")
+    except:
+        c.execute("DROP TABLE purchases;")
+        c.execute("CREATE TABLE purchases (timestamp int, userid int, ean str, price str);")
+    conn.commit()
+    conn.close()
+    return "purchases table created"
+
+
+@app.route('/to_csv', methods=['GET'])
+def write_to_csv():
+    """
+    endpoint to dump db to a csv
+    params (optional): 
+    - userid: userid to return results for
+    - filename: file name to write to
+    """
+    conn = sqlite3.connect('beacon_data.db')
+    c = conn.cursor()
+    userid = request.args.get('userid')
+    filename = request.args.get('filename')
+    if userid is None:
+        res = c.execute("SELECT * FROM proximity;").fetchall()
+    else:
+        res = c.execute("SELECT * FROM proximity WHERE userid = ?;", (userid,)).fetchall()
+    conn.commit()
+    conn.close()
+    df = pd.DataFrame(res)
+    df.columns = ['timestamp', 'userid', 'beacon_id', 'rssi']
+    if filename is None:
+        df.set_index('userid').to_csv('data/test_data.csv')
+    else:
+        df.to_csv(filename)
+    return 'ok'
+
+
+@app.route('/process', methods=['GET'])
+def process_csv():
+    filename = request.args.get('filename')
+    if filename is None:
+        filename = 'data/test_data.csv'
+    from processing.process import process
+    df = process(filename)
+    df.to_csv(filename[:-4] + '_processed.csv')
+    return 'ok'
+
+@app.route('/purchase', methods=['POST'])
+def post_purchase():
+    data = request.get_data(as_text=True)
+    df = pd.read_csv(StringIO(data))
+    print(df)
+    df.columns = ['timestamp', 'userid', 'ean', 'price']
+    data = df
+    conn = sqlite3.connect('beacon_data.db')
+    c = conn.cursor()
+    for i, row in data.iterrows():
+        c.execute("INSERT into purchases VALUES (?,?,?,?);", (int(row.timestamp), int(row.userid), row.ean, row.price))
+    conn.commit()
+    conn.close()
+    return str(data)
+
+@app.route('/purchasedata', methods=['GET'])
+def get_purchases():
+    """
+    get endpoint
+    params (optional): 
+    - userid: userid to return results for
+    """
+    conn = sqlite3.connect('beacon_data.db')
+    c = conn.cursor()
+    userid = request.args.get('userid')
+    if userid is None:
+        res = c.execute("SELECT * FROM purchases;").fetchall()
+    else:
+        res = c.execute("SELECT * FROM purchases WHERE userid = ?;", (userid,)).fetchall()
+    conn.commit()
+    conn.close()
+    return str(res)
+
 
 app.run(host='130.233.87.184')
